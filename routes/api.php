838d354c525593efdd3107b81001cf988755c224
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-07 00:00:12 +0900

    socialiteの設定

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['api'])->group(function() {
+Route::middleware(['api', 'session'])->group(function() {
     Route::get('/hoge', function() {
         return "hoge\n";
     });
