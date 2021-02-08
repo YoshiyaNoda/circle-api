a7c7466200b56c06f03ec9cc3ca13b70583fd71a
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-08 09:51:00 +0900

    認証のコントローラ作成

---
+++ app/Http/Controllers/OAuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OAuthController extends Controller
+{
+    public function retTargetUrl($provider) {
+        return Socialite::driver($provider)->redirect()->getTargetUrl();
+    }
+    public function handleProviderCallback($provider) {
+        $providerUser = Socialite::with($provider)->stateless()->user(); // Laravel\Socialite\Two\InvalidStateException のエラーを解決するためにはこうしないといけないらしい
+
+        try {
+
+            $queries = [
+                'token' => $providerUser->token,
+                'email' => $providerUser->getEmail() ?? '',
+                'name' => $providerUser->getName() ?? ''
+            ];
+
+            $queryString = http_build_query($queries, null, '&');
+
+            return redirect(config('const_env.FRONT_URL')."/auth/finished?".$queryString);
+        } catch(\Exception $e) {
+            return redirect(config('const_env.FRONT_URL')."/auth");
+        }
+    }
+}
